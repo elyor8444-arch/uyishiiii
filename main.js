0dba86e62e4108b87818a03a80ed8dbc98120d58
elyor8444-arch <elyor8444@gmail.com>
2025-08-26 15:43:04 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let arr = ['olma','banan',  'kakos']
+console.log(arr);
+
+
+
+if (confirm (`masivdan malumot ochirmoqchimisiz`)) {
+    arr.pop()
+    console.log(arr);
+    
+} else{
+    console.log(arr);
+}
